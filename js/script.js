7620fa5ab0756ed53efb1e72876c7f3a57362c60
Dessy Mmasi <mmasidesiree9@gmail.comgit config --global user.email Desiree>
2019-03-25 14:35:49 +0300

    Add current changes on my js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ function triangle() {
     if(((sideA+sideB !=sideC) || (sideB+sideC !=sideA) || (sideA+sideC !=sideB)) && ((sideA+sideB<sideC) || (sideB+sideC<sideA) || (sideC+sideA<sideB));
     document.getElementById("result").innerHTML = "Not a Triangle"        
 }
-else if (sideA != sideB && sideB != sideC && sideC != sideA) {
-    document.getElementById("result").innerHTML = "Scalene Triangle"
+else if(sideA != sideB && sideB != sideC && sideC != sideA) {
+    document.getElementById("result").innerHTML = "Scalene Triangle" <img src="Image/Scalene Triangle.png">
 }
-else if (sideA === sideB)
+else if (sideA === sideB && sideB === sideC && sideA === sideC) {
+    document.getElementsById("result").innerHTML = "Equilateral Triangle" <img src="Image/Equilateral Triangle.png>"
+}
+MediaElementAudioSourceNodeee
+
     
 
 
