d2935883410b1f4f126da6d89d46e8fc6a66390b
Dessy Mmasi <mmasidesiree9@gmail.comgit config --global user.email Desiree>
2019-03-25 17:32:26 +0300

    Add changes made on js file

--- js/script.js
+++ js/script.js
@@ -2,23 +2,22 @@ function triangle() {
     var sideA = parseInt(document.getElementById('a').value);
     var sideB = parseInt(document.getElementById('b').value);
     var sideC = parseInt(document.getElementById('c').value);
-    if(((sideA+sideB !=sideC) || (sideB+sideC !=sideA) || (sideA+sideC !=sideB)) && ((sideA+sideB<sideC) || (sideB+sideC<sideA) || (sideC+sideA<sideB));
+    var result = document.getElementsById("result");
+        if(((sideA+sideB !=sideC) || (sideB+sideC !=sideA) || (sideA+sideC !=sideB)) && ((sideA+sideB<sideC) || (sideB+sideC<sideA) || (sideC+sideA<sideB));
     document.getElementById("result").innerHTML = "Not a Triangle";        
 }
-else if (sideA != sideB && sideB != sideC && sideC != sideA) {
-    document.getElementById("result").innerHTML = "Scalene Triangle <img src= Image/Scalene triangle.png">
+else if(sideA != sideB && sideB != sideC && sideC != sideA) {
+    document.getElementById("result").innerHTML = "Scalene Triangle <img src= Image/Scalene triangle.png>"
 }
 else if (sideA === sideB && sideB === sideC && sideA === sideC) {
     document.getElementsById("result").innerHTML = "Equilateral Triangle <img src= Image/Equilateral triangle.png>"
 }
 else if ((sideA === sideB != sideC) || (sideB === sideC != sideA) || (sideC === sideA != sideB)) {
-    document.getElementById("result").innerHTML = "Isosceles Triangle <img src= Image/Isosceles triangle.png">
+    document.getElementById("result").innerHTML = "Isosceles Triangle <img src= Image/Isosceles triangle.png>"
 }
 else{
-    document.getElementById("result").innerHTML = "Not a Triangle <img src= Image/sad.jpg">
+    document.getElementById("result").innerHTML = "Not a Triangle <img src= Image/sad.jpg>"
 }    
-function reload(){
-    location.reload();
-}
+
 
 
