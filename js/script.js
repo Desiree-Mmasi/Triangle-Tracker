52033d04c548d49ddd74d3ae60c7c06d877ccc01
Dessy Mmasi <mmasidesiree9@gmail.comgit config --global user.email Desiree>
2019-03-26 15:56:43 +0300

    Add changes made on the js file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-function triangle(){
-    var sideA =parseInt(document.getElementById('a').value);
-    var sideB =parseInt(document.getElementById('b').value);
-    var sideC =parseInt(document.getElementById('c').value);
-    var response = document.getElementById('response');
-    var triangles = ["Equilateral triangl","Isosceles triangle","Scalene triangle"];
-  
-  
-  if(sideA + sideB > sideC && sideB + sideC > sideA && sideA + sideC >sideB){
-    if (sideA==sideB && sideB==sideC) {
-      response.innerHTML = 'Equilateral Triangle <img src="Image/Equilateral triangle.png">'
-    }
-    else if(sideA!=sideB && sideB!=sideC && sideA!=sideC) {
-      response.innerHTML = 'Scalene Triangle <img src="Image/Scalene Triangle.png">'
-    }
-    else {
-      response.innerHTML = 'Isosceles <img src="Image/Isosceles triangle.png">'
-    }
-  }
-  else if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)){
-      response.innerHTML = 'Not a number <img src="Image/sad.jpg">'
-    }else{
-      response.innerHTML = 'Not a triangle <img src="Image/sad.jpg">'
-  }
-  function reload(){
-    location.reload();
-  }
-  }
+function triangle (a,b,c){
+    if (a+b>c && a+c>b && b+c>a && a==b && b==c)
+alert("Equilateral Triangle"<img src")
+else if (a+b>c && a+c>b && b+c>a && a==b || b==c || c==a)
+alert("Isosceles Triangle")
+else if (a+b>c && a+c>b && b+c>a && a!=b && b!=a && c!=a)
+alert("Scalene Triangle")
+else
+alert("Cannot form a triangle")
+
+} 
